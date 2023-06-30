899436e9b38f5b4b05fc4ee23bbe6667f43273de
Christine Weld <christineweld@tutamail.com>
2023-06-30 11:28:50 -0400

    Lesson 2 6.30.23

---
+++ lesson_2/pseudocode.js
@@ -0,0 +1,50 @@
+/* a function that returns the sum of two numbers
+Start
+Set first number value
+Set second number value
+Function that takes both numbers as arguments, 
+- add the numbers
+- return the sum
+End */
+
+/*a function that takes an array of strings, and returns a string that is all those strings concatenated together 
+START
+- Given array of strings
+- function that takes array as argument
+  - use method to join strings together
+  - return concatenated string
+END */
+
+/* a method that takes an array of integers, and returns a new array with every other element from the original array, starting with the first element.
+START
+SET array = list of integers
+Function everyOther(arrayInt) = 
+  - let newArray = []
+  - newArray.push(FOR interate over the array, i += 2)
+END */
+
+/* a function that determines the index of the 3rd occurrence of a given character in a string. For instance, if the given character is 'x' and the string is 'axbxcdxex', the function should return 6 (the index of the 3rd 'x'). If the given character does not occur at least 3 times, return null. 
+START
+SET = given string
+Function thirdChar(string) = 
+  - For loop: array.filter(['char'])
+    - Let counter = 0, counter <= 3
+    - For i(each character) === 'char', counter +1
+    - when counter = 3, stop
+    - return i
+  - .indexof(i)
+END */
+
+/* a function that takes two arrays of numbers and returns the result of merging the arrays. The elements of the first array should become the elements at the even indexes of the returned array, while the elements of the second array should become the elements at the odd indexes. For instance: 
+START
+SET = array1
+SET = array2
+Function merge(array1, array2) = 
+  - let mixedArray = []
+  - For (i = array1[0], i < array1.length, i +=2) {
+    - mixedArray.push(array1[i])
+  - For (i = array2[1], i < array2.length, i +=2) {
+    - mixedArray.push(array2[i])
+END */
+
+
